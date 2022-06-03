6e936f2a70449b7d795fc8feffa1a04946b96c05
icicle <icicle@icicle.icicle.local>
2022-06-03 10:54:08 -0500

    still trying to set cookie with 2 domains

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,6 +3,7 @@
 const allowedOrigins = [
   `${process.env.REACT_APP__API_IP}:${process.env.REACT_APP__API_PORT}`,
   `${process.env.REACT_APP__API_IP}`,
+  `${process.env.FRONTEND_URL_ORIGIN}:${process.env.FRONTEND_URL_PORT}`,
   `${process.env.FRONTEND_URL_ORIGIN}`,
   'http://127.0.0.1:5500',
   'http://localhost:3500',
